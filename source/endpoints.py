d5492059ef035db7b89f53ec53908256733935cd
Evan <78503987+ep2572@users.noreply.github.com>
2021-03-21 12:10:24 -0400

    Added @api.route '/chatroom'

--- source/endpoints.py
+++ source/endpoints.py
@@ -13,6 +13,7 @@ from source.db import fetch_games
 app = Flask(__name__)
 api = Api(app)
 
+CHATROOM = 'chatroom'
 HELLO = 'hello'
 AVAILABLE = 'Available endpoints:'
 MAIN_MENU = "Main Menu"
@@ -30,7 +31,17 @@ def get_main_menu():
     except FileNotFoundError:
         return None
 
-
+@api.route('/chatroom')
+class Chatroom(Resource)
+    """
+    Attempting to add a new route: Chatroom
+    """
+    def get(self):
+        """
+        For now this will just return a line of text
+        """
+        return {'This is where a ' CHATROOM: ' will be'}
+    
 @api.route('/hello')
 class HelloWorld(Resource):
     """
