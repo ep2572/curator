bcb98103bb08f12b6ff3ce547aed6596952683b4
Evan <78503987+ep2572@users.noreply.github.com>
2021-04-20 13:44:58 -0400

    Update test_endpoints.py

--- source/tests/test_endpoints.py
+++ source/tests/test_endpoints.py
@@ -24,7 +24,7 @@ class TestEndpoints(TestCase):
         Test our "chatroom" endpoint.
         """
         chat_ep = Chatroom(Resource)
-        ret = chat_ep.get()
+        ret = chat_ep.put(12345)
         self.assertIn(ret)
         
     def test_hello(self):
