3ebd756ce3491eb7527e3d47700f8ae1bf130465
Evan <78503987+ep2572@users.noreply.github.com>
2021-09-18 17:15:21 -0400

    Add files via upload

---
+++ source/roomkey.py
@@ -0,0 +1,28 @@
+"""
+---------------------
+Random Roomkey Generator
+---------------------
+"""
+
+import random
+
+alnum = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+keychain = {"" : 1}
+
+
+def getroomkey():
+    random.seed()
+    seq = ""
+    while seq in keychain:
+        seq = ""
+        for n in range(0,12):## Current size of sequence: 12
+            seq += alnum[random.randrange(0,61)]
+    keychain[seq] = 1
+    return seq
+
+
+"""
+Testing
+"""
+##print(getroomkey())
+##print(len(keychain))
