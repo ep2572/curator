31d70fa178591295b5ceebe20c96052027ca6fb0
Evan <78503987+ep2572@users.noreply.github.com>
2021-09-28 14:25:13 -0400

    Added roomkey generator test

--- source/tests/test_db.py
+++ source/tests/test_db.py
@@ -1,10 +1,16 @@
 from unittest import TestCase
 
 from source.db import fetch_games
-
+import roomkey
 
 class DBTestCase(TestCase):
     def test_fetch_games(self):
         games = fetch_games()
         self.assertTrue(isinstance(games, dict))
         self.assertTrue(len(games) > 1)
+
+    def test_gen_roomkey(self):
+        roomkey1 = get_roomkey()
+        roomkey2 = get_roomkey()
+        self.assertTrue(roomkey1.isalnum())
+        self.assertTrue(roomkey2.isalnum())
