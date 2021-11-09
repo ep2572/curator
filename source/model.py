b4a2f341d0b525101742232416139cfd27e1699c
Evan Petersen <ep2572@nyu.edu>
2021-11-09 13:29:14 -0500

    separated database from app.py

--- source/model.py
+++ source/model.py
@@ -1,15 +1,65 @@
-# from . import db
-# from flask_login import UserMixin
-# from sqlalchemy.sql import func
+"""
+===============================================================================
+# Models for the database
+# Users(Many) to Rooms(Many)
+===============================================================================
+"""
 
+from flask_sqlalchemy import SQLAlchemy
 
-# class User(db.Model , UserMixin):
-#
-#
-# class Room(db.Model):
-#
-#
-# class Participant(db.Model):
-#
-#
-# class Message(db.Model):
+db = SQLAlchemy()
+
+KEY_SIZE = 12
+IPV4_MAX_LENGTH = 15
+
+"""
+A client can be in multiple rooms at once, with different names,
+colors, and roles. A new instance of the client class is given
+to the client for each room the are in.
+"""
+class User(db.Model):
+    ip = db.Column(db.String(IPV4_MAX_LENGTH), primary_key=True)
+    name = db.Column(db.String(16), nullable=False)
+    # role: 0 is guest, 1 is moderator, 2 is host
+    role = db.Column(db.SmallInteger, default=0)
+    # Default color is Black 0x000000
+    color = db.Column(db.String(6), default='000000', nullable=False)
+    in_room = db.Column(db.String(KEY_SIZE), db.ForeignKey('room.key'),
+                        primary_key=True)
+    def __repr__(self):
+        return 'User: {}:{}, Role: {}'.format(self.name, self.ip, self.role)
+                     
+"""
+A rooms only exists so long as 1 User is still present. Each room
+will maintain the chat log, 
+"""
+class Room(db.Model):
+    key = db.Column(db.String(KEY_SIZE), primary_key=True)
+    host = db.Column(db.String(IPV4_MAX_LENGTH), nullable=False)
+    name = db.Column(db.String(512), default='curator room '+key)
+    log = db.Column(db.Text, default='')
+    mute = db.Column(db.Boolean, default=False)
+    file = db.Column(db.LargeBinary)
+
+    def __repr__(self):
+        return "{}, {}".format(self.name, self.key)
+
+"""
+Each instance of the Banlist has only one room and one user IP
+"""
+class Banlist(db.Model):
+    room = db.Column(db.String(KEY_SIZE), db.ForeignKey('room.key'), primary_key=True)
+    ip = db.Column(db.String(IPV4_MAX_LENGTH), db.ForeignKey('user.ip'), primary_key=True)
+
+    def __repr__(self):
+        return 'Room: {}, User{}'.format(self.room, self.name)
+
+db.create_all()
+dummy_room = Room(key="",
+                  host="dummy",
+                  name="dummy_room",
+                  log="",
+                  mute=True,
+                  file=None)
+db.session.add(dummy_room)
+db.session.commit()
