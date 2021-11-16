2554f60c75dccbd6316481d464dbedd28d628176
Evan <78503987+ep2572@users.noreply.github.com>
2021-11-16 15:10:57 -0500

    Changed source.model import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, session, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
-from source.model import db
+import source.model
 from source.roomkey import get_roomkey
 import os
 
