5cb2793945fbbc241a9cd455c1ac01324a0ea85e
Evan <78503987+ep2572@users.noreply.github.com>
2021-11-23 13:52:25 -0500

    Removed primary_key from banlist.client

--- source/model.py
+++ source/model.py
@@ -56,8 +56,7 @@ class Banlist(db.Model):
                      db.ForeignKey('room.key'),
                      primary_key=True)
     client = db.Column(db.String(IPV4_MAX_LENGTH),
-                       db.ForeignKey('client.ip'),
-                       primary_key=True)
+                       db.ForeignKey('client.ip'))
 
     def __repr__(self):
         return 'Room: {}, IP{}'.format(self.room, self.client)
