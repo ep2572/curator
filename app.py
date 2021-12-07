83599dc1b7f31cae3684196d54dab386a6539983
Evan Petersen <ep2572@nyu.edu>
2021-12-07 01:07:35 -0500

    Commit remove statement

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ app.app_context().push()
 ##db.create_all()
 open_rooms = db.session.query(db.room)
 open_rooms.delete()
+db.session.commit()
 dummy_room = Room(key="dummy",
                   host="dummy",
                   name="dummy_room",
